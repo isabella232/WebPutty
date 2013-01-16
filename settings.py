befa1328a689345ec080996d763dd302ab11eefd
Navraj Chohan <nlake44@gmail.com>
2013-01-16 01:57:11 +0000

    Moved files because of path issue and set on debug mode because of style sheet issue

--- settings.py
+++ settings.py
@@ -2,20 +2,20 @@
  
 import os
 
-debug = os.environ.get('SERVER_SOFTWARE', '').startswith('Dev')
+debug = True #os.environ.get('SERVER_SOFTWARE', '').startswith('Dev')
 debug_profiler_enabled = False
 appname = 'WebPutty OSS'
 appversion_raw = [1, 3, 0]
 appversion = '.'.join([str(num) for num in appversion_raw])
-invite_sender_email = '%s Invitation <you@example.com>' % appname
-incoming_sender_email = '%s Incoming Mail <you@example.com>' % appname
+invite_sender_email = '%s Invitation <a@a.a>' % appname
+incoming_sender_email = '%s Incoming Mail <a@a.a>' % appname
 log_all_incoming = True
 # List of admins to forward mail to.
-admin_emails = ['you@example.com']
+admin_emails = ['a@a.a']
 forward_mail_to = admin_emails
 jquery_url = '//ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js'
 # Generate this once by calling os.urandom(24)
-secret_key = "Shhh... It's a secret."
+secret_key = "xxx"
 
 # Name of the Google Cloud Storage bucket
 use_google_cloud_storage = False
